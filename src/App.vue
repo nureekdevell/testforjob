529d887ae9ce0d8e98cd9247505c8af6c7875291
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-04-09 02:30:33 +0500

    tree collapse and table

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref } from "vue";
+import TreeCollapse from "./components/TreeCollapse.vue";
+import Table from "./components/Table.vue";
+
+// Tree Collapse
+let tree = ref({
+  label: "MARKA",
+  nodes: [
+    {
+      label: "Daewoo",
+      nodes: [
+        {
+          label: "Nexia",
+          nodes: [
+            {
+              label: "Zapchastlar",
+              nodes: [
+                {
+                  label: "Tugadi",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "BMW",
+      nodes: [
+        {
+          label: "Diskalar",
+        },
+      ],
+    },
+  ],
+});
+
+// Table
+const tableColumns = ["Name", "Age", "Address", "Tags", "Action"];
+const tableData = [
+  {
+    name: "John Doe",
+    age: 32,
+    address: "California",
+    tags: ["nice", "developer"],
+    action: ["invite", "delete", "more"],
+  },
+  {
+    name: "Jane Doe",
+    age: 19,
+    address: "New York",
+    tags: ["loser"],
+    action: ["invite", "delete", "more"],
+  },
+  {
+    name: "Nurjahon Dev",
+    age: 26,
+    address: "Tashkent",
+    tags: ["noob", "developer"],
+    action: ["invite", "delete", "more"],
+  },
+];
+</script>
+
+<template>
+  <main class="container py-10 px-4">
+    <div class="flex w-full justify-center">
+      <Table :columns="tableColumns" :data="tableData" />
+    </div>
+
+    <br />
+    <hr />
+    <br />
+
+    <div class="flex justify-center">
+      <div class="">
+        <TreeCollapse :label="tree.label" :nodes="tree.nodes" />
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/TreeCollapse.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <div class="flex" @click="hideShow">
+      <img class="w-5 h-5" src="../assets/right.png" v-if="!clickHideShow" />
+      <img class="w-5 h-5" src="../assets/down.png" v-else />
+
+      {{ label }}
+    </div>
+    <div v-if="clickHideShow" class="child">
+      <tree-collapse
+        v-for="item in nodes"
+        :label="item.label"
+        :nodes="item.nodes"
+        :key="item.id"
+      ></tree-collapse>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+
+const clickHideShow = ref(false);
+
+const hideShow = () => {
+  clickHideShow.value = !clickHideShow.value;
+};
+
+const props = defineProps({
+  label: String,
+  nodes: Array,
+});
+</script>
+
+<style scoped>
+.child {
+  margin-left: 10px;
+}
+</style>
